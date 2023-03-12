710276687fa4814ab2fed5e2351d8d4a342d54a0
kimhokyung <ghrud92@gmail.com>
2023-03-12 18:13:54 +0900

    Revert "(EDFJ-60) Block commit push to master"
    This reverts commit 0657ec338882a6133cf789f1e95bca00deff2766.

--- App.tsx
+++ App.tsx
@@ -1,23 +1,24 @@
-import SignUpScreen from "./screens/SignUp/SignUpScreen";
-import { NavigationContainer } from "@react-navigation/native";
-import React from "react";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import {SafeAreaProvider} from "react-native-safe-area-context";
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import React from 'react'
+import { SafeAreaProvider } from 'react-native-safe-area-context'
+
+import SignUpScreen from './screens/SignUp/SignUpScreen'
 
 export type StackParamList = {
-  SignUp: undefined;
+  SignUp: undefined
 }
 
-const Stack = createNativeStackNavigator<StackParamList>();
+const Stack = createNativeStackNavigator<StackParamList>()
 
-export default function App() {
+export default function App () {
   return (
     <SafeAreaProvider>
       <NavigationContainer>
         <Stack.Navigator initialRouteName="SignUp">
-          <Stack.Screen name="SignUp" component={SignUpScreen} options={{ headerShown: false }} />
+          <Stack.Screen component={SignUpScreen} name="SignUp" options={{ headerShown: false }} />
         </Stack.Navigator>
       </NavigationContainer>
     </SafeAreaProvider>
-  );
+  )
 }
--- screens/SignUp/SignUpScreen.tsx
+++ screens/SignUp/SignUpScreen.tsx
@@ -1,21 +1,24 @@
-import React from "react";
-import {Text} from "react-native"
-import {NativeStackNavigationProp} from "@react-navigation/native-stack";
-import {StackParamList} from "../../App";
-import {useSafeAreaInsets} from "react-native-safe-area-context";
+import type { NativeStackNavigationProp } from '@react-navigation/native-stack'
+import React from 'react'
+import { Text } from 'react-native'
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
 
-export type SignUpScreenProps = NativeStackNavigationProp<StackParamList, "SignUp">
+import { StackParamList } from '../../App'
+
+export type SignUpScreenProps = NativeStackNavigationProp<StackParamList, 'SignUp'>
 
 const SignUpScreen = (_props: SignUpScreenProps) => {
   const insets = useSafeAreaInsets()
 
   return (
-    <Text style={{
-      paddingTop: insets.top,
-      paddingBottom: insets.bottom,
-      paddingLeft: insets.left,
-      paddingEnd: insets.right,
-    }}>
+    <Text
+      style={{
+        paddingTop: insets.top,
+        paddingBottom: insets.bottom,
+        paddingLeft: insets.left,
+        paddingEnd: insets.right
+      }}
+    >
       sign up screen
     </Text>
   )
