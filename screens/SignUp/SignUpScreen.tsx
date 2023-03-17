f69049421d8434cf7f2af9809ae527c334493f5b
kimhokyung <ghrud92@gmail.com>
2023-03-17 19:50:44 +0900

    (EDFJ-79) 회원가입 페이지 라우팅 추가

--- screens/SignUp/SignUpScreen.tsx
+++ screens/SignUp/SignUpScreen.tsx
@@ -1,8 +1,24 @@
 import React from "react";
-import { Text } from "react-native"
+import {Text} from "react-native"
+import {NativeStackNavigationProp} from "@react-navigation/native-stack";
+import {StackParamList} from "../../App";
+import {useSafeAreaInsets} from "react-native-safe-area-context";
 
-const SignUpScreen = () => {
-  return (<Text>sign up screen</Text>)
+export type SignUpScreenProps = NativeStackNavigationProp<StackParamList, "SignUp">
+
+const SignUpScreen = (_props: SignUpScreenProps) => {
+  const insets = useSafeAreaInsets()
+
+  return (
+    <Text style={{
+      paddingTop: insets.top,
+      paddingBottom: insets.bottom,
+      paddingLeft: insets.left,
+      paddingEnd: insets.right,
+    }}>
+      sign up screen
+    </Text>
+  )
 }
 
 export default SignUpScreen
