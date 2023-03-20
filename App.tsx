dc7a81b47da9a932e035fbb6d02cbff8ec6b5515
supermantoman <jwg7632@gmail.com>
2023-03-21 02:10:51 +0900

    [style] 위치설정 라우트 옵션 추가 및 수정

--- App.tsx
+++ App.tsx
@@ -4,6 +4,7 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import {SafeAreaProvider} from "react-native-safe-area-context";
 import SignUpScreen from './src/screens/SignUp/SignUp/SignUpScreen';
 import LocationSetting from './src/screens/SignUp/LocationSetting/LocationSetting';
+import BackButton from './src/components/BackButton';
 
 export type StackParamList = {
   SignUp: undefined
@@ -17,7 +18,16 @@ export default function App () {
       <NavigationContainer>
         <Stack.Navigator initialRouteName="SignUp">
           {/* <Stack.Screen component={SignUpScreen} name="SignUp" options={{ headerShown: false }} /> */}
-          <Stack.Screen component={LocationSetting} name="LocationSetting" options={{ headerShown: false }} />
+          <Stack.Screen 
+          name="LocationSetting" 
+          component={LocationSetting}
+          options={{ 
+            headerShown: true, 
+            headerLeft: ({navigation}) => <BackButton navigation={navigation} />,
+            headerStyle: {shadowColor: 'transparent' },
+            title: '내 위치 설정하기', 
+          }}
+        />     
         </Stack.Navigator>
       </NavigationContainer>
     </SafeAreaProvider>
