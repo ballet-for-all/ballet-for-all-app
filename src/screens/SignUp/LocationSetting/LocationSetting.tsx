77433bc74eace71431f5c1cff7d7e2626087cc57
supermantoman <jwg7632@gmail.com>
2023-03-21 01:52:53 +0900

    [style] 회원가입 위치설정 페이지 UI 파일 생성 및 import 라이브러리 세팅

---
+++ src/screens/SignUp/LocationSetting/LocationSetting.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import styles from './LocationSettingStyles';
+import { StatusBar } from 'expo-status-bar';
+import { Stylesheet, 
+    View, 
+    Text, 
+    TextInput, 
+    Image, 
+    TouchableOpacity,
+    ScrollView,
+ } from 'react-native';
+
+
+function LocationSetting() {
+
+  return (
+
+    <View style={styles.container}> 
+
+      <StatusBar style="black" />
+
+     
+    </View>
+
+  );
+}
+
+export default LocationSetting;
