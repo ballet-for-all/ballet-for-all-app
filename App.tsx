dc4e92ce88006afd463ef02422e2877f015d8375
supermantoman <jwg7632@gmail.com>
2023-03-21 01:53:43 +0900

    [style] App.js 회원가입 위치설정 페이지 라우팅추가

--- App.tsx
+++ App.tsx
@@ -2,7 +2,8 @@ import { NavigationContainer } from "@react-navigation/native";
 import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import {SafeAreaProvider} from "react-native-safe-area-context";
-import SignUpScreen from './src/screens/SignUp/SignUpScreen';
+import SignUpScreen from './src/screens/SignUp/SignUp/SignUpScreen';
+import LocationSetting from './src/screens/SignUp/LocationSetting/LocationSetting';
 
 export type StackParamList = {
   SignUp: undefined
@@ -15,7 +16,8 @@ export default function App () {
     <SafeAreaProvider>
       <NavigationContainer>
         <Stack.Navigator initialRouteName="SignUp">
-          <Stack.Screen component={SignUpScreen} name="SignUp" options={{ headerShown: false }} />
+          {/* <Stack.Screen component={SignUpScreen} name="SignUp" options={{ headerShown: false }} /> */}
+          <Stack.Screen component={LocationSetting} name="LocationSetting" options={{ headerShown: false }} />
         </Stack.Navigator>
       </NavigationContainer>
     </SafeAreaProvider>
