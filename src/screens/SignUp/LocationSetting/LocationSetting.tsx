b1759d170fabba50da20ced237106ae6db0e95e3
supermantoman <jwg7632@gmail.com>
2023-03-21 02:11:42 +0900

    [style] 회원가입 위치설정 페이지 search bar UI 추가

--- src/screens/SignUp/LocationSetting/LocationSetting.tsx
+++ src/screens/SignUp/LocationSetting/LocationSetting.tsx
@@ -9,6 +9,7 @@ import { Stylesheet,
     TouchableOpacity,
     ScrollView,
  } from 'react-native';
+import setLocationBtn from '@/assets/icons/locationIcon.png';
 
 
 function LocationSetting() {
@@ -18,6 +19,16 @@ function LocationSetting() {
     <View style={styles.container}> 
 
       <StatusBar style="black" />
+
+      <View style={styles.searchWrap}>
+        <TextInput 
+        style={styles.input}
+        placeholder="동이름(읍,면)으로 검색"></TextInput>
+
+      <View>
+        <Image source={setLocationBtn} style={styles.myLocation} />
+      </View>
+    </View>
 
      
     </View>
--- src/screens/SignUp/LocationSetting/LocationSettingStyles.tsx
+++ src/screens/SignUp/LocationSetting/LocationSettingStyles.tsx
@@ -5,6 +5,27 @@ const styles = StyleSheet.create({
     flex: 1,
     position: 'relative',
   },
+  searchWrap: {
+    flex: 0.1,
+    backgroundColor: '#fff',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'row',
+    borderBottomColor: '#eee',
+    borderBottomWidth: 1,
+  },
+  input:{
+    height: 48,
+    backgroundColor: "#eee",
+    padding: 16,
+    width: '70%',
+  },
+  myLocation:{
+    width:56,
+    height:56,
+    resizeMode: 'cover',
+    marginLeft: 8
+  }
 })
 
 export default styles;
